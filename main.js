139b081752eeea38459d86442db73e852c366b8d
Niçoise Nkounkou <nicoise.nkounkou@edu.devinci.fr>
2022-12-30 20:26:52 +0100

    Correction Html + js partie 1

---
+++ main.js
@@ -0,0 +1,47 @@
+const inputs = document.querySelectorAll("input")
+
+inputs.forEach(input=> {
+    input.addEventListener("invalid", handleValidation)
+    input.addEventListener("input", handleValidation)
+})
+
+function handleValidation(e){
+    if(e.type ==="invalid"){
+        e.target.setCustomValidity("Ce champs ne peut être vide !")
+    } else if (e.type ==="input"){
+        e.target.setCustomValidity("")
+    }
+}
+
+const cookieForm = document.querySelector("form")
+
+cookieForm.addEventListener("submit", handleForm)
+
+function handleForm(e){
+    e.preventDefault()
+
+const newCookie = {};
+
+inputs.forEach(input =>{
+  const nameAttribute = input.getAttribute("name")
+  newCookie[nameAttribute]= input.value
+})
+newCookie.expires = new Date(new Date().getTime()+7*24*60*60*1000)
+
+createCookie(newCookie)
+}
+
+function createCookie(newCookie){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
